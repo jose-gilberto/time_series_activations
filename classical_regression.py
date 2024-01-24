11e5da5b986dd8df7f7f2fe4070fdb09b2994ffa
andregdmitri <andregdmitri@gmail.com>
2024-01-24 14:31:02 +0100

    Benchmark for TSER

--- classical_regression.py
+++ classical_regression.py
@@ -1,74 +1,82 @@
 from aeon.datasets._data_loaders import load_regression
-from tsml_eval.experiments import run_regression_experiment
-from tsml_eval.evaluation.storage import load_regressor_results
-from tsml_eval.estimators.regression.convolution_based import MultiRocketHydra
-# from tsml_eval._wip.hc2_regression.hivecote_v2 import HIVECOTEV2
-# from aeon.regression.deep_learning import InceptionTimeRegressor
-from tsml.shapelet_based import RDSTRegressor
-# from  import WEASEL
+from aeon.regression.convolution_based import RocketRegressor
+from aeon.regression.feature_based import Catch22Regressor
+from xgboost import XGBRegressor
+from sklearn.ensemble import RandomForestRegressor
+from tsml.feature_based import FPCARegressor
+from sklearn.metrics import mean_squared_error as mse
+from sklearn.metrics import mean_absolute_error as mae
+import csv
+import numpy as np
 
-# from aeon.datasets.tsc_data_lists import tser_soton as dataset_list
+# from aeon.datasets.tser_data_lists import tser_monash
 
-# This is the tser_soton of the import above
+        
+# This is the tser_data_lists of the import above
 dataset_list = [
     'AppliancesEnergy', 
-    'HouseholdPowerConsumption1',
-    'HouseholdPowerConsumption2',
-    'BenzeneConcentration',
-    'BeijingPM25Quality', 
-    'BeijingPM10Quality', 
-    'LiveFuelMoistureContent', 
-    'FloodModeling1', 
-    'FloodModeling2', 
-    'FloodModeling3', 
-    'AustraliaRainfall', 
-    'IEEEPPG', 
-    'BIDMC32RR', 
-    'BIDMC32HR', 
-    'BIDMC32SpO2', 
-    'NewsHeadlineSentiment', 
-    'NewsTitleSentiment', 
+    # 'HouseholdPowerConsumption1', # May have misisng value problems
+    # 'HouseholdPowerConsumption2',
+    # 'BenzeneConcentration',
+    # 'BeijingPM25Quality', 
+    # 'BeijingPM10Quality', 
+    'LiveFuelMoistureContent',
+    'FloodModeling1',
+    'FloodModeling2',
+    'FloodModeling3',
+    'AustraliaRainfall',
+    'IEEEPPG',
+    'BIDMC32RR',
+    'BIDMC32HR',
+    'BIDMC32SpO2',
+    'NewsHeadlineSentiment',
+    'NewsTitleSentiment',
     'Covid3Month',
 ]
 
-# Estimators/Models
-estimator = {
-    # 'HC2': HIVECOTEV2(),
-    'Hydra-MR': MultiRocketHydra(),
-    # 'InceptionT': InceptionTimeRegressor(),
-    'RDST': RDSTRegressor(),
-    # 'WEASEL': WEASEL_V2(),
-}
-
 # Parameters
 NUM_EXPERIMENTS = 1
+N_JOBS = -1
+RANDOM_STATE = 42
 
+# Models
+estimator = {
+    'RocketRegressor': RocketRegressor(random_state=RANDOM_STATE, n_jobs=N_JOBS),
+    'XGBoost': Catch22Regressor(random_state=RANDOM_STATE, estimator=XGBRegressor(), n_jobs=N_JOBS),
+    'RandomForest': Catch22Regressor(random_state=RANDOM_STATE, estimator=RandomForestRegressor(), n_jobs=N_JOBS),
+    'FPCR': FPCARegressor(n_jobs=N_JOBS),
+}
+
+results_list = [] 
 for dataset_name in dataset_list:
-    X_train, y_train = load_regression(dataset_name, split='train')
-    X_test, y_test = load_regression(dataset_name, split='test')
-    # print(dataset_name)
+    X_train, y_train = load_regression(dataset_name, split='train', load_no_missing=True)
+    X_test, y_test = load_regression(dataset_name, split='test', load_no_missing=True)
 
     for current_model in estimator:
         for experiment in range(NUM_EXPERIMENTS):
-            print('****', current_model, '****')
-            run_regression_experiment(
-                X_train,
-                y_train,
-                X_test,
-                y_test,
-                regressor = estimator[current_model],
-                results_path = "./experiments/",
-                dataset_name=dataset_name,
-                resample_id=0,
-            )
+            model = estimator[current_model].fit(X_train, y_train)
+            preds = model.predict(X_test)
+
+            # Score Metrics
+            mae_value = mae(y_test, preds)
+            mse_value = mse(y_test, preds)
             
-            # # # RESULTS
-            
-            # cr = load_regressor_results(
-            #     f"./experiments/{current_model}/Predictions/{dataset_name}/testResample0.csv"
-            # )
-            # print(cr.predictions)
-            # print(cr.accuracy)
-            # print(cr.balanced_accuracy)
-            # print(cr.auroc_score)
-            # print(cr.log_loss)
+            # Storing the Results
+            result_entry = {
+                'Dataset': dataset_name,
+                'Model': current_model,
+                'Experiment': experiment,
+                'MAE': mae_value,
+                'MSE': mse_value,
+                'RMSE': np.sqrt(mse_value),
+            }
+            results_list.append(result_entry)
+            print(dataset_name, current_model, experiment, mae_value, mse_value)
+
+# Saving Results to csv
+csv_file_path = '../experiments/classical_regression_results.csv'
+with open(csv_file_path, 'w', newline='') as csv_file:
+    writer = csv.DictWriter(csv_file, fieldnames=['Dataset', 'Model', 'Experiment', 'MAE', 'MSE', 'RMSE'])
+    writer.writeheader()
+    for result_entry in results_list:
+        writer.writerow(result_entry)
